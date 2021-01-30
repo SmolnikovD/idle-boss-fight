e85fe8e4533f184e7bfb3563e87675afda506b60
SmolnikovD <smolnikov.d@ukr.net>
2021-01-30 11:32:24 +0200

    Player model and animations added
    Player model and animations added

---
+++ Idle Boss Fight/Assets/Scripts/Player/PlayerAnimation.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    [SerializeField]
+    private Animator playerAnimator;
+
+    Action SetRandomAnimation;
+
+    private string Attack1 = "Attack1";
+    private string Attack2 = "Attack2";
+    private string Idle = "Idle";
+    private string Dizzy = "Dizzy";
+
+    private void Start()
+    {
+        StartCoroutine(RandomAttackAnimationCoroutine());
+    }
+
+    private IEnumerator RandomAttackAnimationCoroutine()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1f);
+            SetRandomAnimation = UnityEngine.Random.Range(0, 2) == 0 ? new Action(SetAttack1Animation) : new Action(SetAttack2Animation);
+            SetRandomAnimation.Invoke();
+        }
+    }
+
+
+    public void SetAttack1Animation()
+    {
+        playerAnimator.SetTrigger(Attack1);
+    }
+
+    public void SetAttack2Animation()
+    {
+        playerAnimator.SetTrigger(Attack2);
+    }
+
+    public void SetIdleAnimation()
+    {
+        playerAnimator.SetTrigger(Idle);
+    }
+
+    public void SetDizzyAnimation()
+    {
+        playerAnimator.SetTrigger(Dizzy);
+    }
+
+}
