41d9f2548679fb033192d3a089bccf7fc7cd4d4f
SmolnikovD <smolnikov.d@ukr.net>
2021-02-03 12:21:03 +0200

    Added Shop UI
    Added Shop UI

---
+++ Idle Boss Fight/Assets/Scripts/UI/ShopUI.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopUI : MonoBehaviour
+{
+    [SerializeField]
+    Animator animator;
+
+    private bool isOpened = false;
+
+    private void Awake()
+    {
+        CloseShop();
+    }
+
+    public void ToggleShop()
+    {
+        if (isOpened) CloseShop();
+        else OpenShop();
+
+        isOpened = !isOpened;
+    }
+
+    private void OpenShop()
+    {
+        animator.ResetTrigger("CloseShop");
+        animator.SetTrigger("OpenShop");
+    }
+
+    private void CloseShop()
+    {
+        animator.ResetTrigger("OpenShop");
+        animator.SetTrigger("CloseShop");
+    }
+}
